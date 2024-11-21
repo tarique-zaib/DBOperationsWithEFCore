42199b92b39b5710025a2227159650c23a63c3ea
tarique-zaib <68943783+tarique-zaib@users.noreply.github.com>
2024-11-21 12:00:08 +0530

    Push

--- DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Data/AppDbContext.cs
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Data/AppDbContext.cs
@@ -6,6 +6,16 @@ namespace DBOperationsWithEFCoreApp.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         { 
-        }        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>().HasData
+            (
+                new Student() { Id = 1, FirstName = "Tarique", LastName = "Zaib", Email = "Tarique.Zaib@gmail.com" }
+            );
+        }
+
+        public DbSet<Student> Student { get; set; }
     }
 }
---
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Migrations/20241121062652_inserteddata.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DBOperationsWithEFCoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class inserteddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Student",
+                columns: new[] { "Id", "Email", "FirstName", "LastName" },
+                values: new object[] { 1, "Tarique.Zaib@gmail.com", "Tarique", "Zaib" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Student",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
