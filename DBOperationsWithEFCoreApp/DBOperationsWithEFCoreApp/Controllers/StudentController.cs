c9e8d6b7cdf04ae1e5fd2c5575b0b78d579ae0bc
tarique-zaib <68943783+tarique-zaib@users.noreply.github.com>
2024-11-21 12:39:23 +0530

    push

---
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using DBOperationsWithEFCoreApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DBOperationsWithEFCoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public StudentController(AppDbContext appDbContext)
+        {
+            this._appDbContext = appDbContext;
+        }
+        [HttpGet("")]
+        public IActionResult GetAllStudent()
+        {
+            var student = _appDbContext.Student.ToList();
+            return Ok(student);
+        }
+
+    }
+}
